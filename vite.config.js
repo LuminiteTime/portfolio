21a2e733cfc4e4d9986d1c54ddc006b859c15dfa
Mikhail <trifonov2812@gmail.com>
2025-02-25 21:37:25 +0300

    HOTFIX: Set localhost as server

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/portfolio/',
+  base: "/portfolio/",
   plugins: [react()],
-})
+  server: {
+    host: "127.0.0.1",
+  },
+});
